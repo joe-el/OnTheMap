acb968e1ef026c1e0b612f8ad4d560ea226fc6f0
Ken <kenneth.joel@live.com>
2022-05-12 17:32:01 +0200

    Initial Commit

---
+++ OnTheMap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by Kenneth Gutierrez on 5/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
